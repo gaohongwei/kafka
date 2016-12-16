247fc8c6419bd1785d74a40d02163950efd1f3e7
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2016-12-16 10:02:21 -0800

    Update build.sh

--- java/build.sh
+++ java/build.sh
@@ -2,5 +2,6 @@
 apt install openjdk-9-jdk-headless maven -y
 git clone https://github.com/gaohongwei/kafka
 
-javac -cp "/opt/kafka/libs/*" SimpleProducer.java
+javac -cp "/opt/kafka/libs/*" *.java
 java  -cp "/opt/kafka/libs/*":. SimpleProducer test
+java  -cp "/opt/kafka/libs/*":. Producer test
