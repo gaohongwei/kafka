3556411b65b04dfe90de7e04ebc67fe51b16dbc7
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2016-12-17 19:16:16 -0800

    Create GateWay.java

---
+++ java/GateWay.java
@@ -0,0 +1,51 @@
+// File Name GateWay.java
+import java.net.*;
+import java.io.*;
+/*
+import com.google.common.io.Resources;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+*/
+public class GateWay {
+   public static void main(String[] args) throws IOException {
+      // set up the producer
+      /*
+      KafkaProducer<String, String> producer;
+      try (InputStream props = Resources.getResource("producer.props").openStream()) {
+         Properties properties = new Properties();
+         properties.load(props);
+         producer = new KafkaProducer<>(properties);
+      }*/
+      int port = 5000;
+      Socket server = null;
+      ServerSocket serverSocket = null;
+      try {
+         serverSocket= new ServerSocket(port);
+         System.out.println("Waiting for client on port " +
+            serverSocket.getLocalPort() + "...");
+         server = serverSocket.accept();
+         System.out.println("Just connected to " + server.getRemoteSocketAddress());
+         DataInputStream in = new DataInputStream(server.getInputStream());
+
+         String textFromClient = null;
+         while ((textFromClient = in.readLine()) != null){
+             System.out.println(textFromClient);
+             //producer.send(new ProducerRecord<String, String>(
+             //           "fast-messages", textFromClient ));
+            // every so often send to a different topic
+         }
+      }catch(SocketTimeoutException s) {
+         System.out.println("Socket timed out!");
+      }catch(Throwable throwable) {
+         System.out.printf("%s", throwable.getStackTrace());
+      } finally {
+         //producer.close();
+         if ( server != null) server.close();
+         System.out.println("Close all");
+      }
+    }
+}
