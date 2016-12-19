363c27ca29428193a59896bcfba8e71bb19e973c
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2016-12-19 10:36:26 -0800

    Create Producer.rb

---
+++ ruby/Producer.rb
@@ -0,0 +1,16 @@
+require 'socket'
+require 'poseidon'
+
+server = TCPServer.new 5000
+producer = Poseidon::Producer.new(["localhost:9092"], "my_test_producer")
+
+client =server.accept
+count = 1
+while(line = client.gets)
+  producer.send_messages(["topic1", line])
+  client.puts 0
+  count+=1
+  puts line if (count%20000 == 0)  
+end
+client.close
+producer.close
