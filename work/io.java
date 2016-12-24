8bb84b7e0d5c88a404ea895d4fe0963bb088cad8
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2016-12-23 18:55:12 -0800

    Update io.java

--- work/io.java
+++ work/io.java
@@ -7,3 +7,13 @@ while ((line = bufferedReader.readLine()) != null)
 }
 // close the BufferedReader when we're done
 bufferedReader.close();
+
+int portNumber = Integer.parseInt(args[0]);
+try ( 
+    ServerSocket serverSocket = new ServerSocket(portNumber);
+    Socket clientSocket = serverSocket.accept();
+    PrintWriter out =
+        new PrintWriter(clientSocket.getOutputStream(), true);
+    BufferedReader in = new BufferedReader(
+        new InputStreamReader(clientSocket.getInputStream()));
+) {
