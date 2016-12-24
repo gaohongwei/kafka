0764b33a78e36bc1cab58b866716572c5053e504
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2016-12-23 16:01:33 -0800

    Update properties.java

--- work/properties.java
+++ work/properties.java
@@ -21,3 +21,18 @@
         props.store(out, "");
         out.close();
     }
+
+import java.net.*;
+import java.io.*;
+public class URLConnectionReader {
+    public static void main(String[] args) throws Exception {
+        URL oracle = new URL("http://www.oracle.com/");
+        URLConnection yc = oracle.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                                    yc.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) 
+            System.out.println(inputLine);
+        in.close();
+    }
+}
